293f6d0f384531e8bdf62cbe347906b4f4b83c82
Drew Clinkenbeard <drew+github@daclink.com>
2011-09-26 14:36:13 -0700

    cloudSuite initial commit

---
+++ c/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "Testing the make file.\n\n";
+
+	return 0;
+}
+
---
+++ cloudSuite/index.php
@@ -0,0 +1,5 @@
+<?php
+include_once 'control/cloud.controller.php';
+include_once 'view/cloud.viewer.php';
+
+?>

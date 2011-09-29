8232dd0616cc5bc4427805182a47a8f3479a1db5
Drew Clinkenbeard <drew+github@daclink.com>
2011-09-28 20:58:46 -0700

    reorganized c folder and started work on the controller

--- cloudSuite/control/cloud.controller.php
+++ cloudSuite/control/cloud.controller.php
@@ -6,12 +6,32 @@
  * @author daclink
  */
 class cloudserver {
+    
+    public function __construct() {
+        ;
+    }
+    
     function test () {
         echo "who!";
     }
+    
+    function startLab ($lab) {
+        //TODO: write startLab
+        //read $lab and send each method request to the server
+    }
 
-
-    //put your code here
+    function getModules () {
+        //read from modules directory and return VLABML
+    }
+    
+    function getLabs () {
+        //read from LABS folder and return VLABML
+    }
+    
+    function getData () {
+        //read from DATA folder and return VLABML        
+    }
+    
 }
 
 ?>

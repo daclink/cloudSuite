eddca307adae2f278032bb712ed13d1c345dea41
Drew Clinkenbeard <drew+github@daclink.com>
2011-11-15 12:33:03 -0800

    basline commit to test push

--- cloudSuite/model/XMLParser.php
+++ cloudSuite/model/XMLParser.php
@@ -1,9 +1,11 @@
 <?php
+
 $doc = new DOMDocument();
+/*
+  #$data = '<?xml version="1.0"?><foo/>';
 
-$data = '<?xml version="1.0"?><foo/>';
-
-$doc->loadXML($data);
+  #$doc->loadXML($data);
+ */
 
 $doc->load('manifest.xml');
 
@@ -11,5 +13,12 @@ if ($doc->schemaValidate('manifest.xsd')) {
     echo 'hey! it worked!';
 } else {
     echo 'well damn.';
+    return;
+}
+
+if ($doc->loadXML('manifest.xml')) {
+    $nodes = $doc->getElementById('node_name');
+    foreach($nodes as $node) {
+    }
 }
 ?>
